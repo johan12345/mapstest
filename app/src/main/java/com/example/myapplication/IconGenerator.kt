ecc067351c28894af9cab3a3f45a17ff414861c0
johan12345 <johan.forstner@gmail.com>
2020-06-28 19:50:49 +0200

    initial commit

---
+++ app/src/main/java/com/example/myapplication/IconGenerator.kt
@@ -0,0 +1,55 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.LruCache
+import com.google.android.libraries.maps.model.BitmapDescriptor
+import com.google.android.libraries.maps.model.BitmapDescriptorFactory
+import kotlin.math.round
+
+class IconGenerator(val context: Context) {
+    val icon = R.drawable.ic_map_marker
+    val cacheSize = 32 * 1024 * 1024; // 32MiB
+    val cache = object : LruCache<Float, Bitmap>(cacheSize) {
+        override fun sizeOf(key: Float, value: Bitmap): Int {
+            return value.byteCount
+        }
+    }
+
+    fun getBitmapDescriptor(scale: Float = 1f): BitmapDescriptor? {
+        val bitmap = generateBitmap(scale)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    private fun generateBitmap(scale: Float): Bitmap {
+        val scaleRounded = round(scale * 20) / 20  // restrict to 20 steps for better caching
+
+        if (cache[scaleRounded] != null) {
+            return cache[scaleRounded]
+        } else {
+            val vd: Drawable = context.getDrawable(icon)!!
+            vd.setBounds(
+                0, 0,
+                vd.intrinsicWidth,
+                vd.intrinsicHeight
+            )
+
+            val bm = Bitmap.createBitmap(
+                (vd.intrinsicWidth).toInt(), (vd.intrinsicHeight).toInt(),
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bm)
+            canvas.scale(
+                scaleRounded,
+                scaleRounded,
+                vd.intrinsicWidth / 2f,
+                vd.intrinsicHeight.toFloat()
+            )
+            vd.draw(canvas)
+            cache.put(scaleRounded, bm)
+            return bm
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -0,0 +1,102 @@
+package com.example.myapplication
+
+import android.animation.ValueAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.animation.addListener
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
+
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.GoogleMap
+import com.google.android.libraries.maps.OnMapReadyCallback
+import com.google.android.libraries.maps.SupportMapFragment
+import com.google.android.libraries.maps.model.LatLng
+import com.google.android.libraries.maps.model.Marker
+import com.google.android.libraries.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var gen: IconGenerator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        gen = IconGenerator(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        for (j in 0..10) {
+            for (i in 0..10) {
+                val pos = LatLng(-34.0 + i * 5, 151.0 + j * 5)
+                MarkerAnimator(gen, mMap, pos).start()
+            }
+        }
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private class MarkerAnimator(val gen: IconGenerator, val map: GoogleMap, val position: LatLng) {
+        private val handler = Handler()
+        private var marker: Marker? = null
+
+        private val removeMarker: Runnable = Runnable {
+            val marker = this.marker ?: return@Runnable
+            animateMarkerDisappear(marker)
+            handler.postDelayed(this.addMarker, 1000)
+        }
+        private val addMarker: Runnable = Runnable {
+            val opts = MarkerOptions().position(position).visible(false)
+            val marker = map.addMarker(opts)
+            animateMarkerAppear(marker)
+            this.marker = marker
+            handler.postDelayed(this.removeMarker, 1000)
+        }
+
+        fun start() {
+            addMarker.run()
+        }
+
+        private fun animateMarkerAppear(marker: Marker) {
+            val anim = ValueAnimator.ofFloat(0f, 1f).apply {
+                duration = 250
+                interpolator = LinearOutSlowInInterpolator()
+                addUpdateListener { animationState ->
+                    val scale = animationState.animatedValue as Float
+                    marker.setIcon(gen.getBitmapDescriptor(scale))
+                    marker.isVisible = true
+                }
+            }
+            anim.start()
+        }
+
+        fun animateMarkerDisappear(marker: Marker) {
+            val anim = ValueAnimator.ofFloat(1f, 0f).apply {
+                duration = 200
+                interpolator = FastOutLinearInInterpolator()
+                addUpdateListener { animationState ->
+                    val scale = animationState.animatedValue as Float
+                    marker.setIcon(gen.getBitmapDescriptor(scale))
+                }
+                addListener(onEnd = {
+                    marker.remove()
+                }, onCancel = {
+                    marker.remove()
+                })
+            }
+            anim.start()
+        }
+    }
+
+
+}
